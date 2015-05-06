b8fc4821c3d347d00382e24035a82986e059b8d4
fortnight <fernando.ellis@gmail.com>
2015-05-06 17:27:19 -0400

    Added some comments to explain how the code works

--- Buttons.py
+++ Buttons.py
@@ -3,11 +3,12 @@ import pygame
 from gi.repository import Gtk
 
 class Buttons:
-    def __init__(self, pos, name, x, y):
+    def __init__(self, pos, name, w, h):
         self.pos = pos
         self.name = name
-        self.size = (x, y)
+        self.size = (w, h)
 
+    # Method to see if the current positon is on a button
     def onButtons(self, pos):
         x, y = pos
         if (self.pos[0] < x and
@@ -18,14 +19,15 @@ class Buttons:
             return pos
         else: return None
 
+    # The event to call when things get clicked on
     def EVENT_CLICK(self):
-        mouseAt = pygame.mouse.get_pos();
-        buttons_event = [
+        mouseAt = pygame.mouse.get_pos();   # see where the mouse currently is
+        buttons_event = [                   # an array of buttons and their events
                             [self.bttnQuit ,self.EVENT_QUIT],
                             [self.bttnPlay , self.EVENT_PLAY],
                             [self.bttnHow ,self.EVENT_HELP],
                         ]
-        for bttn,event in buttons_event:
-            if bttn.onButtons(mouseAt):
-                self.helperRaiseEvent(event)
-                break
+        for bttn,event in buttons_event:    
+            if bttn.onButtons(mouseAt):     # see if the mouse is on the button when called
+                self.helperRaiseEvent(event)# call the event in that place of the array
+                break                       # only call one event at any click (no button overlap)
